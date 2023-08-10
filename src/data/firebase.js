1ff20120a0790f991c6f6705d0e1ea2646284543
Milan Mizera <milan.mizera2323@gmail.com>
2023-08-10 14:24:50 +0200

    Přidání přihlášení google účtem i facebookem ale fb zatím blbne

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getAuth} from "firebase/auth"
+import {getAuth, GoogleAuthProvider} from "firebase/auth"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -20,7 +20,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
 const auth = getAuth(app)
+// předá přihlášení pomocí google účtu
+const googleProvider = new GoogleAuthProvider()
 
-export {app, auth}
+export {app, auth, googleProvider}
--- src/pages/auth/loginPage/Login.jsx
+++ src/pages/auth/loginPage/Login.jsx
@@ -1,11 +1,14 @@
 import "./login.css"
 //react
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Link } from "react-router-dom";
 //databáze
-import { auth } from "../../../data/firebase"
-import { signInWithEmailAndPassword } from "firebase/auth"
+import { auth, googleProvider } from "../../../data/firebase"
+import { signInWithEmailAndPassword, signInWithPopup, FacebookAuthProvider } from "firebase/auth"
 import AuthDetails from "../AuthDetails"
+//ikonky
+import { FcGoogle} from "react-icons/fc";
+import { BsFacebook } from "react-icons/bs";
 
 
 
@@ -14,6 +17,9 @@ export const Login = () => {
 
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+  //přihlášení pomocí googlu
+  const [googleValue, setGoogleValue] = useState("")
+  const [facebookValue, setFacebookValue] = useState("")
 
 
   const signIn = (e) => {
@@ -28,9 +34,47 @@ export const Login = () => {
         console.log(error)
 
       })
-
-
   }
+
+//přihlášení pomocí googlu
+  const signInWithGoogle = () => {
+
+
+    signInWithPopup(auth, googleProvider).then((data)=>{
+
+    setGoogleValue(data.user.email)
+    localStorage.setItem("googlEmail",data.user.email)
+
+    })
+  }
+
+  useEffect(()=> {
+
+setGoogleValue(localStorage.getItem("googleEmail"))
+
+
+  })
+
+  //přihlášení pomocí facebooku
+   const signInWithFacebook = ()=> {
+
+const facebookProvider = new FacebookAuthProvider()
+    signInWithPopup(auth, facebookProvider ).then((data)=>{
+
+      setFacebookValue(data.user.email)
+      localStorage.setItem("facebookEmail",data.user.email)
+  
+      })
+
+   }
+
+   useEffect(()=> {
+
+    setFacebookValue(localStorage.getItem("facebookEmail"))
+    
+    
+      })
+
 
 
   return (
@@ -45,7 +89,14 @@ export const Login = () => {
           <input className="sign-in-input" onChange={(e) => setEmail(e.target.value)} type="email" placeholder="zde zadejte váš email" value={email}></input>
           <input className="sign-in-input" onChange={(e) => setPassword(e.target.value)} type="password" placeholder="zde zadejte vaše heslo" value={password}></input>
           <input className="sign-in-button" type="submit" value="Přihlásit se" ></input>
+          <div className="three-side-sign-in-wrapper">
+          <p className="sign-in-icons-text">Přihlásit se pomocí:</p>
+          <div className="sign-in-icons-wrapper">
+          <FcGoogle className="fb-sign-in-icon" onClick={signInWithGoogle}></FcGoogle>
+         <BsFacebook className="go-sign-in-icon" onClick={signInWithFacebook}></BsFacebook>
+         </div>
           <Link to="/registrace" className="register-text">Nemáte vytvořený účet ? Klikněte zde.</Link>
+          </div>
         </form>
       </div>
 
